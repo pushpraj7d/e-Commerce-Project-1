7a2a042a1c4b58c268014172717993f85eec22dd
pushpraj7d <singhpushparaj123@gmail.com>
2025-08-28 23:24:20 +0530

    axios folder and api url added

---
+++ src/Utils/Context.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Context = () => {
+   
+  const [products, setproducts] = useState(null)  
+  return <div>Context</div>
+  
+}
+
+export default Context
