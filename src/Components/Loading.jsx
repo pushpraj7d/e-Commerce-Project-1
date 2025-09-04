2dca3fe615933b8b54e92b6a73bc6ca9f1375aa0
pushpraj7d <singhpushparaj123@gmail.com>
2025-09-04 23:48:55 +0530

    All Products mapped at home page

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+        <h1 className='text-8xl font-extrabold'>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
--- src/Utils/Context.jsx
+++ src/Utils/Context.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import axios from './axios';
+import React, { createContext, useEffect } from 'react'
 import { useState } from 'react'
+
+export const productContext = createContext();
 
 const Context = (props) => {
    
-  const [products, setproducts] = useState(null)  
-  return <div>
+  const [products, setproducts] = useState(null);
+
+  const getProducts = async()=>{
+    try {
+      const {data} = await axios.get("/products");
+      
+      setproducts(data);
+    } catch (error) {
+      console.log();
+    } 
+  }
+
+  useEffect(()=>{
+    getProducts();
+  }, []);
+
+  return <productContext.Provider value={[products, setproducts]}>
     {props.children}
-  </div>
+  </productContext.Provider>
   
 }
 
